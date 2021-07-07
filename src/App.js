795a245537cf6e9a2b921a8500ab93a08784f404
Vinicius Viana <vinicius@mindsight.com.br>
2021-07-06 23:47:53 -0300

    fix(tests): fix tests

--- src/App.js
+++ src/App.js
@@ -6,7 +6,15 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>I was changed on the Feature Branch!</p>
+        <p>I was changed on the Feature Branch</p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
       </header>
     </div>
   );
